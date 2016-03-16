2da39a2918f44369089298f90654c2c0fa2fa9cc
mla <mla@enonic.com>
2016-03-15 17:13:37 -0700

    Initial commit of files.

---
+++ README.md
@@ -0,0 +1,28 @@
+# LiveChat app for Enonic XP
+
+Have you ever wanted to have a chat with your website visitors? Well now you can. The LiveChat app will add a small chatbox to every page of your site. LiveChat has many exciting features including a ticket system, reports, chat customization, agent groups and more. They even have desktop and mobile apps to streamline your communication. Read more about LiveChat features at [LiveChatInc.com](https://livechatinc.com)
+
+
+## 1. Create your own LiveChat account
+
+If you haven't already done so, visit the [LiveChat](https://livechatinc.com) website and create your own account. New members get a free trial period. You will not need to copy/paste any code.
+
+## 2. Install this app
+
+Go to the Applications tool in your Enonic XP admin console. Click the "Install" button in the toolbar. Select the LiveChat app from the Market. It will be downloaded, installed and started automatically.
+
+## 3. Add this app to your site
+
+Open the Content Studio admin tool. Edit the site content that you want to add chat to. Find LiveChat in the "Applications" selector and click to add it. Now click the pencil icon to open the LiveChat app configuration. Enter the same email address that is used by your LiveChat account.
+
+
+## Compatibility
+
+| Version       | XP version |
+| ------------- | ---------- |
+| 1.0.0         | 6.4.2      |
+
+
+## Copyright and license
+
+This software is licensed under [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0).
---
+++ src/main/resources/site/filters/livechat.js
@@ -0,0 +1,79 @@
+var portal = require('/lib/xp/portal');
+var cacheLib = require('/lib/xp/cache');
+var httpClient = require('/lib/xp/http-client');
+
+var cache = cacheLib.newCache({
+    size: 1
+});
+
+exports.responseFilter = function (req, res) {
+
+    if(req.mode == 'edit') {
+        return res;
+    }
+
+    if(req.params.nocache) {
+        cache.clear();
+    }
+
+    var siteConfig = portal.getSiteConfig() || {};
+    var email = siteConfig.email || null;
+
+    if(!email) {
+        return res;
+    }
+
+    var license = cache.get(email, function() {
+
+        log.info('New LiveChat email cached or the cache was cleared.');
+
+        var response = httpClient.request({
+            url: 'https://api.livechatinc.com/licence/operator/' + email,
+            method: 'GET',
+            connectionTimeout: 5000,
+            readTimeout: 5000,
+            contentType: 'application/json'
+        });
+
+        var data;
+
+        try {
+            data = JSON.parse(response.body);
+        } catch (e) {
+            log.error('LiveChat service error.');
+        }
+
+        if (!data || data.error ) {
+            data = {};
+        }
+        // Cannot return null
+        return data.number || '';
+    });
+
+    if(!license) {
+        return res;
+    }
+
+    var code = "<!-- Start of LiveChat (www.livechatinc.com) code -->" +
+               "<script type='text/javascript'>" +
+               "window.__lc = window.__lc || {};" +
+               "window.__lc.license = " + license + ";" +
+               "(function() {" +
+                 "var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;" +
+                 "lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';" +
+                 "var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);" +
+               "})();" +
+               "</script>" +
+               "<!-- End of LiveChat code -->";
+
+    var bodyEnd = res.pageContributions.bodyEnd;
+
+    if(!bodyEnd) {
+        res.pageContributions.bodyEnd = [];
+    } else if(typeof bodyEnd == 'string') {
+        res.pageContributions.bodyEnd = [ bodyEnd ];
+    }
+    res.pageContributions.bodyEnd.push(code);
+
+    return res;
+};
